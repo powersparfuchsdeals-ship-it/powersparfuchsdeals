16f95f5bcacd4b75bea7d0a626791a8ebc57dab5
powersparfuchsdeals-ship-it <powersparfuchsdeals@gmail.com>
2026-04-30 23:18:14 +0200

    Add TechnikUnter50 page with SEO component

---
+++ pages/amazon-technik-deals.js
@@ -0,0 +1,12 @@
+import SeoCategoryPage from "../components/SeoCategoryPage";
+
+export default function TechnikUnter50() {
+  return (
+    <SeoCategoryPage
+      title="Beste Technik unter 50€"
+      description="Entdecke günstige Technik-Angebote, Gadgets und Amazon Schnäppchen unter 50€."
+      category="all"
+      searchKeywords={["unter 50", "günstig", "angebot", "deal"]}
+    />
+  );
+}
