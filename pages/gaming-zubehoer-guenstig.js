8593396514c8f391897d21a3f4ba685a0aff9c51
powersparfuchsdeals-ship-it <powersparfuchsdeals@gmail.com>
2026-04-30 23:19:19 +0200

    Create gaming-zubehoer-guenstig.js

---
+++ pages/gaming-zubehoer-guenstig.js
@@ -0,0 +1,12 @@
+import SeoCategoryPage from "../components/SeoCategoryPage";
+
+export default function GamingZubehoer() {
+  return (
+    <SeoCategoryPage
+      title="Günstiges Gaming Zubehör"
+      description="Gaming Mäuse, Tastaturen, Headsets und Zubehör mit gutem Preis-Leistungs-Verhältnis."
+      category="gaming"
+      searchKeywords={["gaming", "maus", "tastatur", "headset"]}
+    />
+  );
+}
