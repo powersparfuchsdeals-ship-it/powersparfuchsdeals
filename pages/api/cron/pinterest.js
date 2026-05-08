3519eb849b7110201e1a00344c0bf8c0bbf9b96c
powersparfuchsdeals-ship-it <powersparfuchsdeals@gmail.com>
2026-05-08 21:20:29 +0200

    Update pinterest.js

--- pages/api/cron/pinterest.js
+++ pages/api/cron/pinterest.js
@@ -1,10 +1,14 @@
 export default async function handler(req, res) {
-
   const token = process.env.PINTEREST_ACCESS_TOKEN;
 
-  res.status(200).json({
-    tokenExists: !!token,
-    preview: token?.slice(0, 15)
+  const response = await fetch("https://api.pinterest.com/v5/boards", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 
+  const data = await response.json();
+
+  res.status(response.status).json(data);
 }
